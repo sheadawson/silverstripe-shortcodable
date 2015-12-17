daf10e4cfeee7f6747bb0dd74284d588557b304e
helpfulrobot <chris@silverstripe.com>
2015-12-17 20:42:18 +1300

    Converted to PSR-2

--- code/interfaces/Shortcodable.php
+++ code/interfaces/Shortcodable.php
@@ -7,21 +7,22 @@
  * @package Shortcodable
  * @author shea@livesource.co.nz
  **/
-interface Shortcodable{
+interface Shortcodable
+{
 
-	/**
-	 * Parse the shortcode and render as a string, probably with a template
-	 * @param array $arguments the list of attributes of the shortcode
-	 * @param string $content the shortcode content
-	 * @param ShortcodeParser $parser the ShortcodeParser instance
-	 * @param string $shortcode the raw shortcode being parsed
-	 * @return string
-	 **/
-	public static function parse_shortcode($arguments, $content, $parser, $shortcode);
+    /**
+     * Parse the shortcode and render as a string, probably with a template
+     * @param array $arguments the list of attributes of the shortcode
+     * @param string $content the shortcode content
+     * @param ShortcodeParser $parser the ShortcodeParser instance
+     * @param string $shortcode the raw shortcode being parsed
+     * @return string
+     **/
+    public static function parse_shortcode($arguments, $content, $parser, $shortcode);
 
-	/**
-	 * returns a list of fields for editing the shortcode's attributes
-	 * @return Fieldlist
-	 **/
-	public static function shortcode_attribute_fields();
+    /**
+     * returns a list of fields for editing the shortcode's attributes
+     * @return Fieldlist
+     **/
+    public static function shortcode_attribute_fields();
 }
