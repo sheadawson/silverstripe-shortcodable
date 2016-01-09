55abb1dcca696ae72f7871e48899785cff591211
Peter Thaleikis <petert@catalyst.net.nz>
2016-01-09 17:19:54 +1300

    converting codebase to PSR-2

--- _config.php
+++ _config.php
@@ -1,8 +1,10 @@
 <?php
 
-if(!defined('SHORTCODABLE_DIR')) define('SHORTCODABLE_DIR', rtrim(basename(dirname(__FILE__))));
+if (!defined('SHORTCODABLE_DIR')) {
+    define('SHORTCODABLE_DIR', rtrim(basename(dirname(__FILE__))));
+}
 if (SHORTCODABLE_DIR != 'shortcodable') {
-	throw new Exception('The edit shortcodable module is not installed in correct directory. The directory should be named "shortcodable"');
+    throw new Exception('The edit shortcodable module is not installed in correct directory. The directory should be named "shortcodable"');
 }
 
 // enable shortcodable buttons and add to HtmlEditorConfig
@@ -11,8 +13,8 @@ HtmlEditorConfig::get('cms')->addButtonsToLine(1, 'shortcodable');
 
 // register shortcode parsers for Shortcodable implementors
 foreach (ClassInfo::implementorsOf('Shortcodable') as $class) {
-	$name = $class;
-	ShortcodeParser::get('default')->register($name, array($class, 'parse_shortcode'));
-	// TODO - update SS ShortcodeParser to offer a public api for converting a shortcode to a data array, and use that instead.
-	singleton('ShortcodableParser')->register($name);
+    $name = $class;
+    ShortcodeParser::get('default')->register($name, array($class, 'parse_shortcode'));
+    // TODO - update SS ShortcodeParser to offer a public api for converting a shortcode to a data array, and use that instead.
+    singleton('ShortcodableParser')->register($name);
 }
--- code/extensions/ShortcodableParser.php
+++ code/extensions/ShortcodableParser.php
@@ -1,13 +1,12 @@
 <?php
 /**
  * ShortcodableParser - temporary measure, based on wordpress parser
- * This parser is only used to parse tags in the html editor field for editing in the popup window
+ * This parser is only used to parse tags in the html editor field for editing in the popup window.
+ *
  * @todo update SS ShortcodeParser to offer a public api for converting a shortcode to a data array, and use that instead.
- * @package Shortcodable
  */
 class ShortcodableParser extends Object
 {
-
     protected $shortcodes = array();
 
     public function register($name)
@@ -19,6 +18,7 @@ class ShortcodableParser extends Object
     {
         $pattern = $this->get_shortcode_regex();
         preg_match_all("/$pattern/s", $text, $c);
+
         return $c;
     }
 
@@ -30,13 +30,14 @@ class ShortcodableParser extends Object
         foreach ($keys as $key => $value) {
             $value = trim($values[ $key ], "\"'");
             $type = is_numeric($value) ? 'int' : 'string';
-            $type = in_array(strtolower($value), array( 'true', 'false' )) ? 'bool' : $type;
+            $type = in_array(strtolower($value), array('true', 'false')) ? 'bool' : $type;
             switch ($type) {
                 case 'int': $value = (int) $value; break;
                 case 'bool': $value = strtolower($value) == 'true'; break;
             }
             $c[ $keys[ $key ] ] = $value;
         }
+
         return $c;
     }
 
@@ -44,21 +45,22 @@ class ShortcodableParser extends Object
     {
         $patts = $this->get_pattern($text);
         $t = array_filter($this->get_pattern($text));
-        if (! empty($t)) {
+        if (!empty($t)) {
             list($d, $d, $parents, $atts, $d, $contents) = $patts;
             $out2 = array();
             $n = 0;
-            foreach ($parents as $k=>$parent) {
+            foreach ($parents as $k => $parent) {
                 ++$n;
-                $name = $child ? 'child' . $n : $n;
+                $name = $child ? 'child'.$n : $n;
                 $t = array_filter($this->get_pattern($contents[ $k ]));
                 $t_s = $this->the_shortcodes($out2, $contents[ $k ], true);
-                $output[ $name ] = array( 'name' => $parents[ $k ] );
+                $output[ $name ] = array('name' => $parents[ $k ]);
                 $output[ $name ]['atts'] = $this->parse_atts($atts[ $k ]);
                 $output[ $name ]['original_content'] = $contents[ $k ];
-                $output[ $name ]['content'] = ! empty($t) && ! empty($t_s) ? $t_s : $contents[ $k ];
+                $output[ $name ]['content'] = !empty($t) && !empty($t_s) ? $t_s : $contents[ $k ];
             }
         }
+
         return array_values($output);
     }
 
@@ -66,38 +68,38 @@ class ShortcodableParser extends Object
     {
         $shortcode_tags = $this->shortcodes;
         $tagnames = array_keys($shortcode_tags);
-        $tagregexp = join('|', array_map('preg_quote', $tagnames));
+        $tagregexp = implode('|', array_map('preg_quote', $tagnames));
 
             // WARNING! Do not change this regex without changing do_shortcode_tag() and strip_shortcode_tag()
             // Also, see shortcode_unautop() and shortcode.js.
             return
                       '\\['                              // Opening bracket
-                    . '(\\[?)'                           // 1: Optional second opening bracket for escaping shortcodes: [[tag]]
-                    . "($tagregexp)"                     // 2: Shortcode name
-                    . '(?![\\w-])'                       // Not followed by word character or hyphen
-                    . '('                                // 3: Unroll the loop: Inside the opening shortcode tag
-                    .     '[^\\]\\/]*'                   // Not a closing bracket or forward slash
-                    .     '(?:'
-                    .         '\\/(?!\\])'               // A forward slash not followed by a closing bracket
-                    .         '[^\\]\\/]*'               // Not a closing bracket or forward slash
-                    .     ')*?'
-                    . ')'
-                    . '(?:'
-                    .     '(\\/)'                        // 4: Self closing tag ...
-                    .     '\\]'                          // ... and closing bracket
-                    . '|'
-                    .     '\\]'                          // Closing bracket
-                    .     '(?:'
-                    .         '('                        // 5: Unroll the loop: Optionally, anything between the opening and closing shortcode tags
-                    .             '[^\\[]*+'             // Not an opening bracket
-                    .             '(?:'
-                    .                 '\\[(?!\\/\\2\\])' // An opening bracket not followed by the closing shortcode tag
-                    .                 '[^\\[]*+'         // Not an opening bracket
-                    .             ')*+'
-                    .         ')'
-                    .         '\\[\\/\\2\\]'             // Closing shortcode tag
-                    .     ')?'
-                    . ')'
-                    . '(\\]?)';                          // 6: Optional second closing brocket for escaping shortcodes: [[tag]]
+.'(\\[?)'                           // 1: Optional second opening bracket for escaping shortcodes: [[tag]]
+."($tagregexp)"                     // 2: Shortcode name
+.'(?![\\w-])'                       // Not followed by word character or hyphen
+.'('                                // 3: Unroll the loop: Inside the opening shortcode tag
+.'[^\\]\\/]*'                   // Not a closing bracket or forward slash
+.'(?:'
+                    .'\\/(?!\\])'               // A forward slash not followed by a closing bracket
+.'[^\\]\\/]*'               // Not a closing bracket or forward slash
+.')*?'
+                    .')'
+                    .'(?:'
+                    .'(\\/)'                        // 4: Self closing tag ...
+.'\\]'                          // ... and closing bracket
+.'|'
+                    .'\\]'                          // Closing bracket
+.'(?:'
+                    .'('                        // 5: Unroll the loop: Optionally, anything between the opening and closing shortcode tags
+.'[^\\[]*+'             // Not an opening bracket
+.'(?:'
+                    .'\\[(?!\\/\\2\\])' // An opening bracket not followed by the closing shortcode tag
+.'[^\\[]*+'         // Not an opening bracket
+.')*+'
+                    .')'
+                    .'\\[\\/\\2\\]'             // Closing shortcode tag
+.')?'
+                    .')'
+                    .'(\\]?)';                          // 6: Optional second closing brocket for escaping shortcodes: [[tag]]
     }
 }
