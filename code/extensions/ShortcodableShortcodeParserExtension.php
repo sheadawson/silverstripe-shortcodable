00e47b86e3aec4867d39828a78e8b25cfad570a5
Jonathon Menz <jonomenz@gmail.com>
2016-11-30 18:21:24 -0800

    PHP 5.3 compatibility fix for parser extension

--- code/extensions/ShortcodableShortcodeParserExtension.php
+++ code/extensions/ShortcodableShortcodeParserExtension.php
@@ -4,15 +4,16 @@ class ShortcodableShortcodeParserExtension extends Extension
 {
     public function onBeforeParse(&$content)
     {
+        $parser = $this->owner;
         // Check the shortcode type and convert wrapper to div if block type
         // Regex examples: https://regex101.com/r/bFtD9o/3
         $content = preg_replace_callback(
             '|<p( [^>]*?)?>\s*?\[((.*)([\s,].*)?)\]\s*?</p>|U',
-            function ($matches) {
+            function ($matches) use($parser) {
                 $shortcodeName = $matches[3];
                 // Since we're only concerned with shortcodable objects we know the
                 // shortcode name will be the class name so don't have to look it up
-                return ($shortcodeName && $this->owner->registered($shortcodeName)
+                return ($shortcodeName && $parser->registered($shortcodeName)
                     && Config::inst()->get($shortcodeName, 'shortcodable_is_block'))
                     ? "<div$matches[1]>[$matches[2]]</div>"
                     : $matches[0];
